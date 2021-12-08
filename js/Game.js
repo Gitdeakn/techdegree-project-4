1f5e370d5aa47742036308174a62b3a0d0044475
GitDeakn <89800630+GitDeakn@users.noreply.github.com>
2021-12-08 13:27:52 -0800

    update js

--- js/Game.js
+++ js/Game.js
@@ -4,6 +4,7 @@ class Game {
         this.phrases = this.createPhrases()
         this.activePhrase = null;
     }
+
     // Creates phrases for use in the game returns an array of phrases
     createPhrases() {
         const arrayOne = ['Phrase One', 'Phrase Two', 'Phrase Three', 'Phrase Four', 'Phrase Five'];
@@ -11,12 +12,14 @@ class Game {
         arrayOne.forEach(phrase => arrayTwo.push(new Phrase(phrase)));
         return arrayTwo;
     }
+
     // Selects random phrase from phrases property
     // return {Object} Phrase object chosen to be used
     getRandomPhrase() {
         const randomNumber = Math.floor(Math.random() * this.phrases.length)
         return this.phrases[randomNumber];
     }
+
     // Begins game by selecting a random phrase and displaying it to user
     startGame() {
         const overlay = document.querySelector('#overlay');
@@ -24,10 +27,10 @@ class Game {
         this.activePhrase = game.getRandomPhrase();
         this.activePhrase.addPhraseToDisplay();
     }
+
     // This takes in the button clicked by the player 
     // If letter clicked it shows letter on the gameboard
     // If letter clicked does not match is removes a life from the gameboard
-
     handleInteraction(button) {
         console.log('handle fired')
         button.disabled = true;
@@ -38,16 +41,12 @@ class Game {
                 this.gameOver(true);
             }
         } else {
-            // if (this.missed < 5) {
             button.classList.add('wrong')
             this.removeLife()
-            // } else {
-            //     this.gameOver(false)
-            // }
         }
     }
 
-    //     // Checks to see if the player has revealed all of the letters
+    // Checks to see if the player has revealed all of the letters
     checkForWin() {
         const phraseLetters = document.querySelectorAll('.hide', '.letter')
         if (phraseLetters.length === 0) {
@@ -62,18 +61,17 @@ class Game {
         const hearts = document.querySelectorAll('.tries')
         const heart = hearts[this.missed]
         this.missed++;
-        if (this.missed < 5){
-        let img = heart.children[0]
-        let src = img.getAttribute('src')
-        if (src === 'images/liveHeart.png') {
-            img.removeAttribute('images/liveHeart.png')
-            img.setAttribute('src', 'images/lostHeart.png')
+        if (this.missed < 5) {
+            let img = heart.children[0]
+            let src = img.getAttribute('src')
+            if (src === 'images/liveHeart.png') {
+                img.removeAttribute('images/liveHeart.png')
+                img.setAttribute('src', 'images/lostHeart.png')
+            }
+        } else {
+            this.gameOver(false)
         }
-        
-    } else {
-        this.gameOver(false)
-    }
-        
+
     }
 
     // Displays start screen with a message based on win or loss
@@ -90,17 +88,18 @@ class Game {
             this.resetGame()
         }
     }
-
+    
+    // Resets the gameboard 
+    // Resets the players lifes for when new game starts
+    // Removes the chosen and wrong class on the gameboard keys
+    // Resets missed property to 0
+    // Deletes the html in ul
     resetGame() {
         const ul = document.querySelector('ul');
         const phraseLetters = document.querySelectorAll('.key');
         const hearts = document.querySelectorAll('.tries');
-        // console.log(hearts)
         ul.innerHTML = '';
         this.missed = 0;
-        // console.log(phraseLetters);
-        // console.log(ul);
-        // console.log(this.missed)
         console.log(phraseLetters)
         phraseLetters.forEach(letter => {
             letter.classList.remove('chosen', 'wrong')
--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 let game;
 const startGameButton = document.querySelector('#btn__reset');
 
+// When start button is clicked we update game to the Game class and call startGame
 startGameButton.addEventListener('click', () => {
     game = new Game();
-    game.startGame();
-    
+    game.startGame(); 
 });
 
+// Waits for user to click a phrase letter key then calls the handleInteraction method on the game object
 const keys = document.querySelectorAll('.key')
     keys.forEach(element => element.addEventListener('click', (e) => {
         game.handleInteraction(e.target)  
